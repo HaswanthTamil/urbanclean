d48f026048eef6b366f8386a090fde5ea31e65d0
vikashsekar <vikashsekar8807@gmail.com>
2026-02-17 11:17:09 +0530

    procesing plant instructions

--- core/models/processing_plant.py
+++ core/models/processing_plant.py
@@ -1,17 +1,66 @@
 from location import Location
-from 
 
 class ProcessingPlant:
-    def __init__(self, id: str, plant_type: str, location: Location, pollution_limit: int):
+    def __init__(self, id :int,max_capacity :float, plant_type: str, location: Location, pollution_limit: int):
         self.id = id
         self.type = plant_type
         self.location = location
+        self.max_capacity=max_capacity
+        self.current_capacity=0
         self.pollution_limit = pollution_limit
         self.current_pollution = 0
         self.is_operational = True
+    
+    """
+    def biodegradable(self,volume:float):
+        if volume is greater than the biodegradable plant
+        than the data,pass to next plant,and existing volume to
+        if data from json compare,if greater,:
+            return"""
+    """
+    def non_biodegradable(self,volume:float):
+        waste_type=self.type
+    
+        if waste_type=="metal":
+            if data from json compare,if greater,:
+                return
+                add to the data of this class
+        if waste_type=="e-waste":
+            if data from json compare,if greater,:
+                return
+               add to the data of this class 
+        if waste_type=="medical":
+            if data from json compare,if greater,:
+                return
+               add to the data of this class         
+        if waste_type=="inert":
+            if data from json compare,if greater,:
+                return
+                add to the data of this class    
+        if waste_type=="others":
+            if data from json compare,if greater,:
+                return
+               add to the data of this class    
+        if waste_type=="recyclable":
+            if data from json compare,if greater,:
+                return
+               add to the data of this class """
+    
 
-    def output_towards_destination(waste_type :str):
-        
+    def output_towards_destination(self,volume :int):
+        waste_type=self.type
+        if waste_type in ["recylable","metal","e-waste"]:
+            """pass to recycling factory,given volume,intoa truck
+             with apt direction"""
+        elif waste_type=="medical":
+            """pass to medical recycle,given volume,intoa truck
+             with apt direction"""  
+        else:
+            """pass to dumpyard,given volume,intoa truck
+             with apt direction"""      
+
+
+
 
 
 
