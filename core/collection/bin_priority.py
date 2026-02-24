1aba5166407b117c1361723d35a85f018be95ee3
vikashsekar <vikashsekar8807@gmail.com>
2026-02-24 11:06:23 +0530

    bin priority function done

--- core/collection/bin_priority.py
+++ core/collection/bin_priority.py
@@ -1,5 +1,6 @@
 from models.bin import Bin
-from utils.storage import load_json
+from utils.storage import *
+from utils.path import *
 '''10.emergency
  highly populated/slums
  commercial area
@@ -14,3 +15,47 @@ industry
 
 '''import data from json'''
 '''raw bin-load->compare loc.id->compare with location type->assign pripority->priority bins'''
+datarawbin = []
+datalocation = []
+datafinal = []
+
+load_json(LOCATION_DATA, datalocation)
+load_json(RAW_BIN_DATA, datarawbin)
+
+for bin in datarawbin:
+    for location in datalocation:
+        if bin["location_id"] == location["id"]:
+
+            if location["location_type"] == "emergency":
+                bin["priority"] = 10
+
+            elif location["location_type"] == "populated":
+                bin["priority"] = 9
+
+            elif location["location_type"] == "commercial":
+                bin["priority"] = 8
+
+            elif location["location_type"] == "tourist":
+                bin["priority"] = 7
+
+            elif location["location_type"] == "school":
+                bin["priority"] = 6
+
+            elif location["location_type"] == "urban-loc":
+                bin["priority"] = 5
+
+            elif location["location_type"] == "govt-building":
+                bin["priority"] = 4
+
+            elif location["location_type"] == "private-corp":
+                bin["priority"] = 3
+
+            else:
+                bin["priority"] = 1   # default priority
+
+            datafinal.append(bin)
+datafinal.sort(key=lambda x: x["priority"], reverse=True)
+save_json(PRIORITY_BIN_DATA,datafinal)   
+ 
+                            
+                        
