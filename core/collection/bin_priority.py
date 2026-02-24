3c077dd57266a9a2b89e8cec7d0e692ab4be9129
vikashsekar <vikashsekar8807@gmail.com>
2026-02-24 10:22:12 +0530

    Resolve merge conflict

---
+++ core/collection/bin_priority.py
@@ -0,0 +1,16 @@
+from models.bin import Bin
+from utils.storage import load_json
+'''10.emergency
+ highly populated/slums
+ commercial area
+tourists spots
+ schools/colleges 
+urban locality
+govt buildings
+private companies
+industry
+
+'''
+
+'''import data from json'''
+'''raw bin-load->compare loc.id->compare with location type->assign pripority->priority bins'''
